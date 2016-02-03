4fc1931d48bddd93c7082f04d5b181ae1b599e5b
DESAROLLO <desarollo@imac-de-francisco.local>
2016-02-03 16:40:11 -0600

    Initial Commit

---
+++ SignalRApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignalRApp
+//
+//  Created by DESAROLLO on 03/02/16.
+//  Copyright © 2016 GTM-DESAROLLO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignalRApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignalRApp
+//
+//  Created by DESAROLLO on 03/02/16.
+//  Copyright © 2016 GTM-DESAROLLO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
